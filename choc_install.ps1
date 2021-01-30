e29118d071bd2e122148122925f8985a1caac7b1
PietJankbal <xerox.xerox2000x@gmail.com>
2021-01-30 10:35:22 +0100

    Update choc_install.ps1
    fix errors

--- choc_install.ps1
+++ choc_install.ps1
@@ -127,20 +127,36 @@ if(Test-Path 'env:CHOC_INSTALL_ALL'){
 
 if(Test-Path 'env:SCOOP_INSTALL'){
 
-    (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/a/9/4/a94c5d25-3195-43dc-8dbe-28e1a87e1b59/Windows6.0-KB936330-X64-wave1.exe", "$env:TEMP\\Windows6.0-KB936330-X64-wave1.exe")
+    Write-Host "Downloading and installing adk, this may take quite some time..."
+
+    (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/6/A/E/6AEA92B0-A412-4622-983E-5B305D2EBE56/adk/adksetup.exe", "$env:TEMP\\adksetup.exe")
+
+    Start-Process winecfg.exe  -Wait -ArgumentList "/v win7" 
+
+    Start-Process adksetup.exe  -ArgumentList "/quiet /features OptionId.WindowsPreinstallationEnvironment"
+    $adkid = (Get-Process adksetup).id; Wait-Process -Id $adkid;
+
+    Copy-Item "${env:ProgramFiles`(x86`)}\\Windows Kits\\8.1\\Assessment and Deployment Kit\\Windows Preinstallation Environment\\amd64\\en-us\\winpe.wim" "$env:TEMP\\winpe64.wim"
+
+    Copy-Item "${env:ProgramFiles`(x86`)}\\Windows Kits\\8.1\\Assessment and Deployment Kit\\Windows Preinstallation Environment\\x86\\en-us\\winpe.wim" "$env:TEMP\\winpe32.wim"
 
 
-    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","$env:TEMP\\Windows6.0-KB936330-X64-wave1.exe","-o$env:TEMP","amd64_microsoft-windows-robocopy_31bf3856ad364e35_6.0.6001.18000_none_2325cb04a4c1adef/robocopy.exe"
-    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","$env:TEMP\\Windows6.0-KB936330-X64-wave1.exe","-o$env:TEMP","x86_microsoft-windows-robocopy_31bf3856ad364e35_6.0.6001.18000_none_c7072f80ec643cb9/robocopy.exe"
 
-    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","$env:TEMP\\Windows6.0-KB936330-X64-wave1.exe","-o$env:TEMP","amd64_microsoft-windows-mfc42x_31bf3856ad364e35_6.0.6001.18000_none_4f3369c50dec2a50/mfc42u.dll"
-    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","$env:TEMP\\Windows6.0-KB936330-X64-wave1.exe","-o$env:TEMP","x86_microsoft-windows-mfc42x_31bf3856ad364e35_6.0.6001.18000_none_f314ce41558eb91a/mfc42u.dll"
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","$env:TEMP\\winpe64.wim","-o$env:TEMP","Windows/WinSxS/amd64_microsoft-windows-robocopy_31bf3856ad364e35_6.3.9600.16384_none_b7c58f8bc05b432d/Robocopy.exe"
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "e","$env:TEMP\\winpe32.wim","-o$env:TEMP","Windows/WinSxS/x86_microsoft-windows-robocopy_31bf3856ad364e35_6.3.9600.16384_none_5ba6f40807fdd1f7/Robocopy.exe"
 
-    Copy-Item -Path "$env:TEMP\\amd64_microsoft-windows-robocopy_31bf3856ad364e35_6.0.6001.18000_none_2325cb04a4c1adef\\robocopy.exe" -Destination "$env:SystemRoot\\system32\\robocopy.exe"
-    Copy-Item -Path "$env:TEMP\\x86_microsoft-windows-robocopy_31bf3856ad364e35_6.0.6001.18000_none_c7072f80ec643cb9\\robocopy.exe" -Destination "$env:SystemRoot\\syswow64\\robocopy.exe"
- 
-    Copy-Item -Path "$env:TEMP\\amd64_microsoft-windows-mfc42x_31bf3856ad364e35_6.0.6001.18000_none_4f3369c50dec2a50\\mfc42u.dll" -Destination "$env:SystemRoot\\system32\\mfc42u.dll"
-    Copy-Item -Path "$env:TEMP\\x86_microsoft-windows-mfc42x_31bf3856ad364e35_6.0.6001.18000_none_f314ce41558eb91a\\mfc42u.dll" -Destination "$env:SystemRoot\\syswow64\\mfc42u.dll"
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","$env:TEMP\\winpe64.wim","-o$env:TEMP","Windows/WinSxS/amd64_microsoft-windows-mfc42x_31bf3856ad364e35_6.3.9600.16384_none_e3d32e4c2985bf8e/mfc42u.dll"
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "e","$env:TEMP\\winpe32.wim","-o$env:TEMP","Windows/WinSxS/x86_microsoft-windows-mfc42x_31bf3856ad364e35_6.3.9600.16384_none_87b492c871284e58/mfc42u.dll"
+
+
+    $7zid = (Get-Process 7z).id; Wait-Process -Id $7zid;
+
+    Copy-Item -Path "$env:TEMP\\Robocopy.exe" -Destination "$env:SystemRoot\\syswow64\\robocopy.exe"
+    Copy-Item -Path "$env:TEMP\\Windows\\WinSxS\\amd64_microsoft-windows-robocopy_31bf3856ad364e35_6.3.9600.16384_none_b7c58f8bc05b432d\\Robocopy.exe" -Destination "$env:SystemRoot\\system32\\robocopy.exe"
+
+    Copy-Item -Path "$env:TEMP\\mfc42u.dll" -Destination "$env:SystemRoot\\syswow64\\mfc42u.dll"
+    Copy-Item -Path "$env:TEMP\\Windows\\WinSxS\\amd64_microsoft-windows-mfc42x_31bf3856ad364e35_6.3.9600.16384_none_e3d32e4c2985bf8e\\mfc42u.dll" -Destination "$env:SystemRoot\\system32\\mfc42u.dll"
+
 
     Start-Process  "pwsh.exe" -ArgumentList "-c iwr -useb get.scoop.sh | iex"
 
@@ -149,6 +165,7 @@ if(Test-Path 'env:SCOOP_INSTALL'){
     Start-Process  "pwsh.exe" -ArgumentList "-c scoop install 7zip"
 
 }
+
 
 
 
