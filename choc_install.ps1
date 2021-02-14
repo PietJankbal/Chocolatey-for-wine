8ced1221cd92e8b9e71af0a89697537bb60868d9
PietJankbal <xerox.xerox2000x@gmail.com>
2021-02-14 17:58:33 +0100

    Update choc_install.ps1

--- choc_install.ps1
+++ choc_install.ps1
@@ -38,10 +38,10 @@ if(Test-Path 'env:SCOOP_INSTALL'){
 #7z.exe x -r -aoa
 
     Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "rn","$env:TEMP\\winpe32.wim","Windows/System32","Windows/syswow64"
-    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
+#    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
     Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","-r","$env:TEMP\\winpe32.wim","-aoa","-o$env:SystemDrive","Windows"
-    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
-    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","-r,""$env:TEMP\\winpe64.wim","-aoa","-o$env:SystemDrive","Windows"
+#    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","-r","$env:TEMP\\winpe64.wim","-aoa","-o$env:SystemDrive","Windows"
     Get-Process 7z | Foreach-Object { $_.WaitForExit() }
     #$7zid = (Get-Process 7z).id; Wait-Process -Id $7zid;
 
