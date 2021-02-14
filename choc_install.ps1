3c9aafcd256509035e4222c98f2085333db8e98a
PietJankbal <xerox.xerox2000x@gmail.com>
2021-02-14 18:33:30 +0100

    Update choc_install.ps1

--- choc_install.ps1
+++ choc_install.ps1
@@ -36,11 +36,18 @@ if(Test-Path 'env:SCOOP_INSTALL'){
 
  #   Remove-Item –path c:\windows –recurse
 #7z.exe x -r -aoa
+#7z d archive.7z -r dir/
+
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "d","$env:TEMP\\winpe32.wim","-r","Windows/WinSxS/"
+
+    Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "d","$env:TEMP\\winpe64.wim","-r","Windows/WinSxS/"
+
+    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
 
     Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "rn","$env:TEMP\\winpe32.wim","Windows/System32","Windows/syswow64"
-#    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
+    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
     Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","-r","$env:TEMP\\winpe32.wim","-aoa","-o$env:SystemDrive","Windows"
-#    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
+    Get-Process 7z | Foreach-Object { $_.WaitForExit() }
     Start-Process ${env:ProgramFiles}\\7-zip\\7z.exe  -ArgumentList "x","-r","$env:TEMP\\winpe64.wim","-aoa","-o$env:SystemDrive","Windows"
     Get-Process 7z | Foreach-Object { $_.WaitForExit() }
     #$7zid = (Get-Process 7z).id; Wait-Process -Id $7zid;
