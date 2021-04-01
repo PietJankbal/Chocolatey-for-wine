c91b5236fefbadbd83ec21f91e7a4e1af4b5d4c0
PietJankbal <xerox.xerox2000x@gmail.com>
2021-04-02 00:32:09 +0200

    Update choc_install.ps1

--- choc_install.ps1
+++ choc_install.ps1
@@ -1320,7 +1320,7 @@ $msil_files = (`
            $DIRNAME=$Xml.assembly.assemblyIdentity.name #System.Managment.Automation
 	    Write-Host dirname is "$DIRNAME" 
           #C:\windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35"C:\windows\assembly\GAC_MSIL\System.Manageent.Automation\1.0.0.0__31bf3856ad364e35
-           $ABS_PATH='$env:systemroot\assembly\GAC_MSIL\' + '$DIRNAME\1.0.0.0__' + '$MSILTOKEN'
+           $ABS_PATH= "$env:systemroot\assembly\GAC_MSIL\\" + "$DIRNAME\1.0.00_" +"$MSILTOKEN"
            Write-Host ABSPATH is "$ABSPATH"
           if (-not (Test-Path -Path "$ABSPATH" )) { New-Item -Path "$ABSPATH" -ItemType directory -Force}
 	  
