e0f74c591ee20dd91c72578a924b504519fd1110
PietJankbal <xerox.xerox2000x@gmail.com>
2021-04-18 08:35:45 +0200

    Update choc_install.ps1

--- choc_install.ps1
+++ choc_install.ps1
@@ -1,7 +1,5 @@
     [System.IO.Directory]::SetCurrentDirectory("$env:TEMP")
-    #$wc=new-object system.net.webclient
-    #$wc.UseDefaultCredentials = $true
-    #$wc.downloadfile("your_url","your_file")
+
     (New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z1900-x64.exe", "$env:TEMP\\7z1900-x64.exe")
     (New-Object System.Net.WebClient).DownloadFile("http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsserver2003-kb968930-x64-eng_8ba702aa016e4c5aed581814647f4d55635eff5c.exe", "$env:TEMP\\windowsserver2003-kb968930-x64-eng_8ba702aa016e4c5aed581814647f4d55635eff5c.exe")
     (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe", "$env:TEMP\\dotNetFx40_Full_x86_x64.exe")
@@ -9,11 +7,7 @@
 
     Start-Process -FilePath 7z1900-x64.exe -Wait -ArgumentList "/S"
 
-
-    
-
 #if(Test-Path 'env:ROTZOOI'){
-
     Copy-Item -Path "$env:windir\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe" -Destination "$env:windir\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell_orig.exe"
     Copy-Item -Path "$env:winsysdir\\WindowsPowerShell\\v1.0\\powershell.exe" -Destination "$env:winsysdir\\WindowsPowerShell\\v1.0\\powershell_orig.exe"
 
@@ -24,9 +18,6 @@
     Start-Process  "winecfg.exe" -Wait -ArgumentList "/v win7"
     $winecfgid = (Get-Process winecfg).id; Wait-Process -Id $winecfgid
 
-
- 
-
     Start-Process -FilePath ${env:ProgramFiles}\\7-zip\\7z.exe -Wait -ArgumentList  "x windowsserver2003-kb968930-x64-eng_8ba702aa016e4c5aed581814647f4d55635eff5c.exe wow64/powershell.exe"
     Start-Process -FilePath ${env:ProgramFiles}\\7-zip\\7z.exe -Wait -ArgumentList "x windowsserver2003-kb968930-x64-eng_8ba702aa016e4c5aed581814647f4d55635eff5c.exe powershell.exe"
 
@@ -36,29 +27,21 @@
     Copy-Item -Path "$env:windir\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell_orig.exe" -Destination "$env:windir\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe"
     Copy-Item -Path "$env:winsysdir\\WindowsPowerShell\\v1.0\\powershell_orig.exe" -Destination "$env:winsysdir\\WindowsPowerShell\\v1.0\\powershell.exe"
 
+    function set_HKLM_SM_key() <# sets key under HKLM:\\Software\\Microsoft #>
+    {
+        Param ($path, $name, $val, $prop) 
+        $HKLM_SM = 'HKLM:\\Software\\Microsoft'; $HKLM_SM_WOW = 'HKLM:\\Software\\Wow6432Node\\Microsoft'
+        New-ItemProperty -Path "$(Join-Path $HKLM_SM $path)" -Name  $name -Value $val -PropertyType $prop -force
+        $newpath = "$(Join-Path $HKLM_SM_WOW $path)" -replace 'system32','syswow64'
+        New-ItemProperty -Path "$newpath" -Name  $name -Value $val -PropertyType $prop -force
+    }
 
-
-
-function set_HKLM_SM_key()
-{
- Param ($path, $name, $val, $prop) 
- $HKLM_SM = 'HKLM:\\Software\\Microsoft'
-  $HKLM_SM_WOW = 'HKLM:\\Software\\Wow6432Node\\Microsoft'
- New-ItemProperty -Path "$(Join-Path $HKLM_SM $path)" -Name  $name -Value $val -PropertyType $prop -force
-   $newpath = "$(Join-Path $HKLM_SM_WOW $path)" -replace 'system32','syswow64'
- New-ItemProperty -Path "$newpath" -Name  $name -Value $val -PropertyType $prop -force
-}
-
-
-function new_HKLM_SM_key()
-{
- Param ($path) 
- $HKLM_SM = 'HKLM:\\Software\\Microsoft'
-  $HKLM_SM_WOW = 'HKLM:\\Software\\Wow6432Node\\Microsoft'
-
- New-Item -Path "$(Join-Path $HKLM_SM $path)" -force
- New-Item -Path "$(Join-Path $HKLM_SM_WOW $path)" -force
-}
+    function new_HKLM_SM_key()  <# creates key under HKLM:\\Software\\Microsoft #>
+    {
+         Param ($path) 
+         $HKLM_SM = 'HKLM:\\Software\\Microsoft'; $HKLM_SM_WOW = 'HKLM:\\Software\\Wow6432Node\\Microsoft'
+         New-Item -Path "$(Join-Path $HKLM_SM $path)" -force;  New-Item -Path "$(Join-Path $HKLM_SM_WOW $path)" -force
+    }
 
     #Try fix wrong registrykeys due to bug https://bugs.winehq.org/show_bug.cgi?id=25740
     New-ItemProperty -Path 'HKLM:\\Software\\Classes\\Microsoft.PowerShellConsole.1\\shell\\open\\command' -force -Name '(Default)' -Value   'c:\\windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -p %1 '  -PropertyType 'String' 
@@ -91,158 +74,67 @@ function new_HKLM_SM_key()
     set_HKLM_SM_key 'HKLM:\\System\\CurrentControlSet\\Services\\WinRM' 'ImagePath' 'c:\\windows\\system32\\svchost.exe -k WINRM' 'String'
     #Install choco
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
-
-
 #}end ROTZOOI
-
-
     #/* Install dotnet48 otherwise choco fails to install packages; procedure copied from winetricks */
-
     #/* remove_mono */
-
-#FIXME!! Needs to be updated when Mono updates; Find better solution...........
-$f = uninstaller --list  | Select-String 'Mono'
-$g = $f -split "\|" |Select-string "{"
-Uninstaller --remove $g[0]
-Uninstaller --remove $g[1]
-
-   # Start-Process uninstaller -Wait -ArgumentList  "--remove", "{0A7C8977-1185-5C3F-A4E7-7A90611227C3}"
-   # Start-Process uninstaller -Wait -ArgumentList "--remove", "{05C9CD26-9144-58FC-8A6E-B4DE47B661EC}"
-    # Get-Process uninstaller | Foreach-Object { $_.WaitForExit() }
-
-   # Remove-Item -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5' -Recurse
-  #  Remove-Item -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v4'  -Recurse  
-
-  #  Remove-Item -Path 'HKLM:\\Software\\Wow6432Node\\Microsoft\\NET Framework Setup\\NDP\\v3.5' -Recurse
-  #  Remove-Item -Path 'HKLM:\\Software\\Wow6432Node\\Microsoft\\NET Framework Setup\\NDP\\v4'  -Recurse  
+    $f = uninstaller --list  | Select-String 'Mono'
+    $g = $f -split "\|" |Select-string "{"
+    uninstaller --remove $g[0]
+    uninstaller --remove $g[1]
 
     Remove-Item -Path "$env:SystemRoot\\SysWOW64\\mscoree.dll" -Force
     Remove-Item -Path "$env:SystemRoot\\System32\\mscoree.dll" -Force
     #/* END remove_mono */
     
- 
-
-
-
-    #New-Item -Path 'HKCU:\\Software\\Wine\\DllOverrides'
     New-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -force -Name 'mscorwks' -Value 'native' -PropertyType 'String'
     New-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -force -Name 'mscoree' -Value 'native' -PropertyType 'String'
 
-    #Remove-Item -Path "$env:SystemRoot\\SysWOW64\\mscoree.dll" #-Force
-    #Remove-Item -Path "$env:SystemRoot\\System32\\mscoree.dll" #-Force
-
-
-    Start-Process winecfg.exe  -Wait -ArgumentList "/v win7" 
     New-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -force -Name 'fusion' -Value 'builtin' -PropertyType 'String'
-#//    New-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -force -Name 'mscoree' -Value 'native' -PropertyType 'String'
 
     Start-Process winecfg.exe  -Wait -ArgumentList "/v winxp64"
-
     Start-Process dotNetFx40_Full_x86_x64.exe  -Wait -ArgumentList "/q /c:install.exe /q"
     $dotnet40id = (Get-Process dotNetFx40_Full_x86_x64).id; Wait-Process -Id $dotnet40id
 
     Start-Process winecfg.exe  -Wait -ArgumentList "/v win7" 
     Remove-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -Name 'fusion'
-
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full' -Name 'Install' -Value '0001' -PropertyType 'DWord'
- #   New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full' -Name 'Version' -Value '4.0.30319' -PropertyType 'String'
-
     #/*FIXME FIXME commented out for now, installation already takes >5 minutes without this*/
     #system(" start /WAIT %SystemDrive%\\windows\\Microsoft.NET\\Framework\\v4.0.30319\\ngen.exe executequeueditems "
 
-    #/*FIXME FIXME  add norestart????*/
     Start-Process ndp48-x86-x64-allos-enu.exe  -Wait -ArgumentList "sfxlang:1027 /q /norestart"
     $dotnet48id = (Get-Process ndp48-x86-x64-allos-enu).id; Wait-Process -Id $dotnet48id
 
-    #"${WINE}" reg add "HKLM\\Software\\Microsoft\\.NETFramework" /v OnlyUseLatestCLR /t REG_DWORD /d 0001 /f
     New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\.NETFramework' -Name 'OnlyUseLatestCLR' -Value '0001' -PropertyType 'DWord'
     New-ItemProperty -Path 'HKLM:\\Software\\Wow6432Node\\Microsoft\\.NETFramework' -Name 'OnlyUseLatestCLR' -Value '0001' -PropertyType 'DWord'
 
-
-#    New-Item -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0' -Name 'Install' -Value '1' -PropertyType 'DWord'
-##    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0' -Name 'SP' -Value '2' -PropertyType 'DWord'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0' -Name 'Version' -Value '3.2.30729' -PropertyType 'String'
     new_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0' 'Install' '1' 'dword'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0' 'SP' '2' 'dword'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0' 'Version' '3.2.30729' 'string'
 
-#    New-Item -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0\\Setup'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0\\Setup' -Name 'InstallSuccess' -Value '1' -PropertyType 'DWord'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.0\\Setup' -Name 'Version' -Value '3.2.30729' -PropertyType 'String'
     new_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0\\Setup'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0\\Setup' 'InstallSuccess' '1' 'dword'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0\\Setup' 'Version' '3.2.30729' 'string'
-
-
-#    New-Item -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5' -Name 'Install' -Value '1' -PropertyType 'DWord'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5' -Name 'SP' -Value '2' -PropertyType 'DWord'
-#    New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5' -Name 'Version' -Value '3.5.30729.4926' -PropertyType 'String'
 
     new_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.5'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.5' 'Install' '1' 'dword'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.5' 'SP' '2' 'dword'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.0' 'Version' '3.5.30729.4926' 'string'
 
-
-    #New-Item -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5\\1033'
-    #New-ItemProperty -Path 'HKLM:\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5\\1033' -Name 'Install' -Value '1' -PropertyType 'DWord'
     new_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.5\\1033'
     set_HKLM_SM_key 'NET Framework Setup\\NDP\\v3.5\\1033' 'Install' '1' 'dword'
-
-##[HKEY_CURRENT_USER\Software\Wine\Fonts\Replacements]
-#"Lucida Console"="Tahoma"
-
+    <# workarounds for powershell_ise #>
     New-Item -Path 'HKCU:\\Software\\Wine\\Fonts\\Replacements'
     New-ItemProperty -Path 'HKCU:\\Software\\Wine\\Fonts\\Replacements' -Name 'Lucida Console' -Value 'Tahoma' -PropertyType 'String'
-
-#HKEY_CURRENT_USER\SOFTWARE\Microsoft\Avalon.Graphics\DisableHWAcceleration
- New-Item -Path 'HKCU:\\Software\\Microsoft\\Avalon.Graphics' -force
- New-ItemProperty -Path 'HKCU:\\Software\\Microsoft\\Avalon.Graphics' -Name 'DisableHWAcceleration' -Value '1' -PropertyType 'dword'  
-#    New-ItemProperty -Path 'HKCU:\\Software\\Wine\\AppDefaults\\powershell_ise.exe\\DllOverrides' -force -Name 'd3d9' -Value 'disable' -PropertyType 'String'
-
-#[HKEY_LOCAL_MACHINE\Software\Microsoft\NET Framework Setup\NDP\v3.0]
-#"Increment"="01"
-#"Install"=dword:00000001
-#"MSI"=dword:00000001
-#"SP"=dword:00000002
-#"Version"="3.2.30729"
-
-
-#[HKEY_LOCAL_MACHINE\Software\Microsoft\NET Framework Setup\NDP\v3.5]
-#"Install"=dword:00000001
-#"InstallPath"="C:\\windows\\Microsoft.NET\\Framework64\\v3.5\\"
-#"MSI"=dword:00000001
-#"SP"=dword:00000001
-#"Version"="3.5.30729.01"
-
-#[HKEY_LOCAL_MACHINE\Software\Microsoft\NET Framework Setup\NDP\v3.0\Setup]
-#"InstallSuccess"=dword:00000001
-#"Version"="3.2.30729"
-
-
-
-
-
-
-
+    New-Item -Path 'HKCU:\\Software\\Microsoft\\Avalon.Graphics' -force
+    New-ItemProperty -Path 'HKCU:\\Software\\Microsoft\\Avalon.Graphics' -Name 'DisableHWAcceleration' -Value '1' -PropertyType 'dword'  
+    <# END workarounds for powershell_ise #>
 
     Copy-Item -Path "$env:windir\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe" -Destination "$env:windir\\SysWOW64\\wusa.exe" -Force
     Copy-Item -Path "$env:winsysdir\\WindowsPowerShell\\v1.0\\powershell.exe" -Destination "$env:winsysdir\\wusa.exe" -Force
-#    Copy-Item -Path "$env:winsysdir\\WindowsPowerShell\\v1.0\\powershell.exe" -Destination "$env:winsysdir\\wusadummy.exe" -Force
-#    Copy-Item -Path "$env:windir\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe" -Destination "$env:windir\\SysWOW64\\wusadummy.exe" -Force
-
-#http://vcloud-lab.com/entries/powershell/powershell-invoke-webrequest-the-request-was-aborted-could-not-create-ssl-tls-secure-channel
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-#[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-
-    #Import-Module -Name C:\Windows\System32\WindowsPowerShell\v1.0\Modules\NetTCPIP -Verbose
 
     New-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -force -Name 'wusa.exe' -Value 'native' -PropertyType 'String'
     New-ItemProperty -Path 'HKCU:\\Software\\Wine\\DllOverrides' -force -Name 'd3dcompiler_47' -Value 'native' -PropertyType 'String'
-
     #Start-Process  "winecfg.exe" -Wait -ArgumentList "/v win81"
     Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show('Chocolatey installed','Congrats','ok','exclamation')
+    
     powershell.exe
