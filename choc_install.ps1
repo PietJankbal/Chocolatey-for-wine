dc766c9d72f734345f6080bd99da45833abac1b4
PietJankbal <xerox.xerox2000x@gmail.com>
2021-09-26 11:44:49 +0200

    Update choc_install.ps1

--- choc_install.ps1
+++ choc_install.ps1
@@ -171,5 +171,8 @@
    # refreshenv
 
     Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show('Chocolatey installed','Congrats','ok','exclamation')
-    
+    # choco feature disable --name='powershellHost'
+    #Start-Process choco.exe -Wait -ArgumentList  "feature", "disable","--name='powershellHost'","-NoNewWindow"
+    #$chocoid = (Get-Process choco).id; Wait-Process -Id $chocoid
+    'c:\\ProgramData\\chocolatey\\choco.exe feature disable --name=powershellHost'
     powershell.exe
