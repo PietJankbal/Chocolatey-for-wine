bd8c37534aca798885709c11843f38c54915059c
PietJankbal <xerox.xerox2000x@gmail.com>
2022-03-25 14:44:52 +0100

    add code to dismiss conemu fast configuration window

--- choc_install.ps1
+++ choc_install.ps1
@@ -111,3 +111,164 @@
     
     # choco install tccle -y; & "$env:ProgramFiles\\JPSoft\\TCCLE14x64\\tcc.exe" "$env:ProgramFiles\\JPSoft\\TCCLE14x64\\tccbatch.btm";
     powershell.exe
+    
+    # following code is only to dismiss ConEmu`s annoying fast configuration window, by sending "enter" keystroke to it
+
+# add a C# class to access the WIN32 API SetForegroundWindow
+Add-Type @"
+
+ using System;
+ using System.Runtime.InteropServices;
+
+ public class StartActivateProgramClass {
+ [DllImport("user32.dll")]
+ [return: MarshalAs(UnmanagedType.Bool)]
+ public static extern bool SetForegroundWindow(IntPtr hWnd);
+ }
+
+"@
+
+# get the applications with the specified title
+$p = Get-Process | Where-Object { $_.MainWindowTitle -Match "fast configuration" }
+while(!$p) {Sleep 1}
+
+# get the window handle of the first application
+$h = $p[0].MainWindowHandle
+
+# set the application to foreground
+[void] [StartActivateProgramClass]::SetForegroundWindow($h)
+
+Add-Type @"
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public static class Keystroke_Synthesizer {    
+    public enum InputType : uint {
+        INPUT_MOUSE = 0,
+        INPUT_KEYBOARD = 1,
+        INPUT_HARDWARE = 3
+    }
+
+    [Flags]
+    internal enum KEYEVENTF : uint
+    {
+        KEYDOWN = 0x0,
+        EXTENDEDKEY = 0x0001,
+        KEYUP = 0x0002,
+        SCANCODE = 0x0008,
+        UNICODE = 0x0004
+    }
+
+    [Flags]
+    internal enum MOUSEEVENTF : uint
+    {
+        ABSOLUTE = 0x8000,
+        HWHEEL = 0x01000,
+        MOVE = 0x0001,
+        MOVE_NOCOALESCE = 0x2000,
+        LEFTDOWN = 0x0002,
+        LEFTUP = 0x0004,
+        RIGHTDOWN = 0x0008,
+        RIGHTUP = 0x0010,
+        MIDDLEDOWN = 0x0020,
+        MIDDLEUP = 0x0040,
+        VIRTUALDESK = 0x4000,
+        WHEEL = 0x0800,
+        XDOWN = 0x0080,
+        XUP = 0x0100
+    }
+
+    // Master Input structure
+    [StructLayout(LayoutKind.Sequential)]
+    public struct lpInput {
+        internal InputType type;
+        internal InputUnion Data;
+        internal static int Size { get { return Marshal.SizeOf(typeof(lpInput)); } }            
+    }
+
+    // Union structure
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct InputUnion {
+        [FieldOffset(0)]
+        internal MOUSEINPUT mi;
+        [FieldOffset(0)]
+        internal KEYBDINPUT ki;
+        [FieldOffset(0)]
+        internal HARDWAREINPUT hi;
+    }
+
+    // Input Types
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MOUSEINPUT
+    {
+        internal int dx;
+        internal int dy;
+        internal int mouseData;
+        internal MOUSEEVENTF dwFlags;
+        internal uint time;
+        internal UIntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct KEYBDINPUT
+    {
+        internal short wVk;
+        internal short wScan;
+        internal KEYEVENTF dwFlags;
+        internal int time;
+        internal UIntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct HARDWAREINPUT
+    {
+        internal int uMsg;
+        internal short wParamL;
+        internal short wParamH;
+    }
+
+    private class unmanaged {
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern uint SendInput (
+            uint cInputs, 
+            [MarshalAs(UnmanagedType.LPArray)]
+            lpInput[] inputs,
+            int cbSize
+        );
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern short VkKeyScan(char ch);
+    }
+
+    internal static uint SendInput(uint cInputs, lpInput[] inputs, int cbSize) {
+        return unmanaged.SendInput(cInputs, inputs, cbSize);
+    }
+
+    public static void SendKeyStroke() {
+        lpInput[] KeyInputs = new lpInput[2];
+        lpInput KeyInput = new lpInput();
+        // Generic Keyboard Event
+        KeyInput.type = InputType.INPUT_KEYBOARD;
+        KeyInput.Data.ki.wScan = 0;
+        KeyInput.Data.ki.time = 0;
+        KeyInput.Data.ki.dwExtraInfo = UIntPtr.Zero;
+
+        KeyInput.Data.ki.wVk = 13; //Enter
+        KeyInput.Data.ki.dwFlags = KEYEVENTF.KEYDOWN;
+        KeyInputs[0] = KeyInput;
+
+        KeyInput.Data.ki.wVk = 13;
+        KeyInput.Data.ki.dwFlags = KEYEVENTF.KEYUP;
+        KeyInputs[1] = KeyInput;
+
+        SendInput(2, KeyInputs, lpInput.Size);
+
+        return;
+        }
+}
+"@
+
+     [Keystroke_Synthesizer]::SendKeyStroke()
+
+    
