d6f1eedd47039bf383b532de08841191b9dda552
PietJankbal <xerox.xerox2000x@gmail.com>
2022-04-07 11:36:03 +0200

    add example how to intercept exe's

--- profile.ps1
+++ profile.ps1
@@ -101,3 +101,19 @@ function c:\windows\system32\tasklist.exe.QPR
 {    
     Get-WmiObject win32_process "processid,name" | Format-Table -Property Name, processid -autosize
 }
+# This is how to intercept any non-wine executable (here csc.exe)
+# For ease of use the copying of fake csc.exe is already done during installation process
+# For any exe it should likely be done manually like here:
+#     cd ~/.wine/drive_c/windows/Microsoft.NET/Framework/v4.0.30319/
+# Backup the real exe with right suffix
+#     cp -rf ./csc.exe ./csc.exe.QPR
+# Copy dummy over the exe (take care whether it`s 64 or 32-bit, here 32-bit exe is replaced)
+#     cp -rf ~/.wine/drive_c/windows/syswow64/WindowsPowerShell/v1.0/powershell.exe ./csc.exe
+# Now we can intercept the program like below; try 'wine csc.exe' or just 'csc' from powershell console
+Set-Alias csc.exe c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe.QPR; Set-Alias csc c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe.QPR
+function c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe.QPR
+{    
+    Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show('Intercepted call!','Congrats','ok','exclamation')
+    # Might manipulate commandline arguments here, or return whatever hack you want. For now just continue.
+    Start-Process  -NoNewWindow -Wait c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe.QPR $args
+}
