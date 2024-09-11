d5f62b5e25654c51b4f3739ff403379b098ec306
PietJankbal <xerox.xerox2000x@gmail.com>
2024-09-11 19:41:29 +0200

    Update mainv1.c

--- mainv1.c
+++ mainv1.c
@@ -15,22 +15,22 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  *
  * Build: // For fun I changed code from standard main(argc,*argv[]) to something like https://nullprogram.com/blog/2016/01/31/ and https://scorpiosoftware.net/2023/03/16/minimal-executables/)
- * x86_64-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -municode -Wall -Wextra -Wl,-gc-sections mainv1.c -nostdlib -lucrtbase -lkernel32 -s -o powershell64.exe && strip -R .reloc powershell64.exe 
-   i686-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -mno-stack-arg-probe -municode -Wall -Wextra -Wl,-gc-sections mainv1.c -nostdlib -lucrtbase -lkernel32 -s -o powershell32.exe && strip -R .reloc powershell32.exe
+ * x86_64-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -municode -Wall -Wextra -Wl,-gc-sections mainv1.c -nostdlib -lucrtbase -lkernel32 -s -o powershell64.exe && strip -R .reloc powershell64.exe
+   i686-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -mno-stack-arg-probe -municode -Wall -Wextra -Wl,-gc-sections mainv1.c -nostdlib -lucrtbase -lkernel32 -s -o powershell32.exe && strip -R .reloc  powershell32.exe
  */
- 
+
 #include <wchar.h>
 #include <windows.h>
 #include <winternl.h>
 
-/* clbuf = incoming cmdline */ 
+/* clbuf = incoming cmdline */
 #define clbuf peb->ProcessParameters->CommandLine.Buffer
 /* for e.g. -noprofile, -nologo , -mta etc */
 static BOOL is_single_option(WCHAR* opt) { return !!wcschr(L"nNmMsS", opt[1]); }
 /* no new options may follow -c, -f , -e (but not -ex(ecutionpolicy)! and '-') */
 static BOOL is_last_option(WCHAR* opt) { return ((wcschr(L"cCfFeE\0", opt[1]) && _wcsnicmp(&opt[1], L"ex", 2) && _wcsnicmp(&opt[1], L"config", 6))); }
 /* join strings with a space in between */
-static __attribute__ ((noinline)) void join(WCHAR* string1, WCHAR* string2) { if(string2) wcscat(wcscat(string1, L" "), string2); }
+static __attribute__((noinline)) void join(WCHAR* string1, WCHAR* string2) { if (string2) wcscat(wcscat(string1, L" "), string2); }
 
 int mainCRTStartup(PPEB peb) {
     wchar_t pwsh[MAX_PATH], ps51[MAX_PATH], file[_MAX_FNAME], *cl = calloc(4096, sizeof(wchar_t)); /* cl = new outgoing cmdline */
@@ -46,31 +46,32 @@ int mainCRTStartup(PPEB peb) {
     WCHAR* cmd = (clbuf[0] == '"') ? wcschr(clbuf + 1, L'"') + 1 : wcschr(clbuf, L' ');
 
     /* I can also act as a dummy program if my exe-name is not powershell, allows to replace a system exe (like wusa.exe, or any exe really) by a function in profile.ps1 */
-    if (_wcsnicmp(file, L"Powershell", 10)) {         /* note: set desired exitcode in the function in profile.ps1 */
-        wcscat(wcscat(cl, L"-nop -c QPR."), file);    /* add some prefix to the exe and execute it through pwsh , so we can query for program replacement in profile.ps1 */
-        SetEnvironmentVariableW(L"QPRCMDLINE", cmd);  /* track cmdline via $env:QPRCMDLINE ($MyInvocation.Line seems to remove quotes (!)) */
+    if (_wcsnicmp(file, L"Powershell", 10)) {        /* note: set desired exitcode in the function in profile.ps1 */
+        wcscat(wcscat(cl, L"-nop -c QPR."), file);   /* add some prefix to the exe and execute it through pwsh , so we can query for program replacement in profile.ps1 */
+        SetEnvironmentVariableW(L"QPRCMDLINE", cmd); /* track cmdline via $env:QPRCMDLINE ($MyInvocation.Line seems to remove quotes (!)) */
     } else {
         /* Main program: pwsh requires a command option "-c" , powershell doesn`t;  insert it e.g. 'powershell -nologo 2+1' should go into 'powershell -nologo -c 2+1'*/
         WCHAR *ptr = 0, delim = L' ', *token = (cmd ? wcstok_s(cmd, &delim, &ptr) : 0); /* Start breaking up cmdline to look for options */
         /* Break up cmdline manually (as CommandLineToArgVW seems to remove some (double) qoutes) */
         while (token) {
-            if (token[0] == L'/') token[0] = L'-';                            /* deprecated '/' still works in powershell 5.1, replace to simplify code */
- 
-            if (token[0] != '-' || is_last_option(token)) { /* no further options in cmdline, or final {-c, -f ,-enc, -} : no new options may follow  these */
-                if ((token[0] != '-' && _waccess(token, 0)) || (token[0] == L'-' && !token[1]) )
-                    join(cl, L"-c");                                          /* insert '-c' if necessary (no option, no file, or '-')*/
-                join(cl, token);                                              /* add arg */
-                join(cl, ptr);                                                /* add remainder of cmdline and done */
+            if (token[0] == L'/') token[0] = L'-'; /* deprecated '/' still works in powershell 5.1, replace to simplify code */
+
+            if (token[0] != '-' || is_last_option(token)) {                                     /* no further options in cmdline, or final {-c, -f ,-enc, -} : no new options may follow  these */
+                if ((token[0] != '-' && _waccess(token, 0)) || (token[0] == L'-' && !token[1])) /* insert '-c' if necessary (no option, no file, or '-')*/
+                    join(cl, L"-c");
+                join(cl, token);                                                                /* add arg */
+                join(cl, ptr);                                                                  /* add remainder of cmdline and done */
                 break;
             }
-            
-             if (is_single_option(token)) {                                   /* e.g. -noprofile, -nologo , -mta etc */
-                if (_wcsnicmp(token, L"-nop", 4)) join(cl, token);            /* skip -noprofile to alays enable hacks in profile.ps1 */
-            } else {                                                          /* assuming option + argument (e.g. '-executionpolicy bypass') AND a valid command!!!, no check for garbage commands!!!!!! */
-                if (!_wcsnicmp(token, L"-ve", 3))                             /* skip incompatible version option, like '-version 3.0' */
-                    token = wcstok_s(NULL, &delim, &ptr);
-                else {                                                        /* concatenate option + arg for option with argument */
+
+            if (is_single_option(token)) {        /* e.g. -noprofile, -nologo , -mta etc */
+                if (_wcsnicmp(token, L"-nop", 4)) /* skip -noprofile to alays enable hacks in profile.ps1 */
                     join(cl, token);
+            } else {                              /* assuming option + argument (e.g. '-executionpolicy bypass') AND a valid command!!!, no check for garbage commands!!!!!! */
+                if (!_wcsnicmp(token, L"-ve", 3)) /* skip incompatible version option, like '-version 3.0' */
+                    token = wcstok_s(NULL, &delim, &ptr);
+                else {
+                    join(cl, token);              /* concatenate option + arg for option with argument */
                     token = wcstok_s(NULL, &delim, &ptr);
                     join(cl, token);
                 }
@@ -78,7 +79,6 @@ int mainCRTStartup(PPEB peb) {
             token = wcstok_s(NULL, &delim, &ptr);
         }
     }
-    
     // /*track the cmd:*/ FILE *fptr; fptr = fopen("c:\\log.txt", "a");fputws(L"used commandline is now: ",fptr); fputws(cl,fptr); fclose(fptr);
     CreateProcessW(_wgetenv(L"PS51") ? ps51 : pwsh, cl, 0, 0, 0, 0, 0, 0, &si, &pi);
     WaitForSingleObject(pi.hProcess, INFINITE);GetExitCodeProcess(pi.hProcess, &exitcode);CloseHandle(pi.hProcess);CloseHandle(pi.hThread);
