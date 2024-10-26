ed5523bbb60da546a72e3d9426e51e303dfe2d63
PietJankbal <xerox.xerox2000x@gmail.com>
2024-10-26 23:25:27 +0200

    Update mainv1.c

--- mainv1.c
+++ mainv1.c
@@ -1,21 +1,27 @@
 /* Wraps cmdline into correct syntax for pwsh.exe + code allowing calls to an exe (like wusa.exe) to be replaced by a function in profile.ps1
  *
- * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  Lesser General Public License for more details.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  * 
- * To make dummy program handle redirected input, insert after line 51:   FILE_FS_DEVICE_INFORMATION info;IO_STATUS_BLOCK io;HANDLE input=GetStdHandle(STD_INPUT_HANDLE);\
- * NtQueryVolumeInformationFile(input,&io,&info,sizeof(info),FileFsDeviceInformation );if(info.DeviceType==17||info.DeviceType==8){wcscat(cl, L" ");while(fgetws(cl+wcslen(cl),4096,stdin)!=NULL)continue;SetEnvironmentVariableW(L"QPRCMDLINE",cl+17);}
- * Build: For fun I changed code from standard main(argc,*argv[]) to something like https://nullprogram.com/blog/2016/01/31/ and https://scorpiosoftware.net/2023/03/16/minimal-executables/)
- * x86_64-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -municode -Wall -Wextra -mno-stack-arg-probe -finline-limit=0 -Wl,-gc-sections -Xlinker --stack=0x100000,0x100000 mainv1.c -nostdlib -lucrtbase -lkernel32 -lntdll -s -o powershell64.exe && strip -R .reloc powershell64.exe
-   i686-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -mno-stack-arg-probe -municode -Wall -Wextra -mno-stack-arg-probe -finline-limit=0 -Wl,-gc-sections -Xlinker --stack=0x100000,0x100000 mainv1.c -nostdlib -lucrtbase -lkernel32 -lntdll -s -o powershell32.exe && strip -R .reloc  powershell32.exe
+ * To make dummy program handle redirected input, insert after line 56:   FILE_FS_DEVICE_INFORMATION info;IO_STATUS_BLOCK io;HANDLE input=GetStdHandle(STD_INPUT_HANDLE);\
+ * NtQueryVolumeInformationFile(input,&io,&info,sizeof(info),FileFsDeviceInformation );if(info.DeviceType==17||info.DeviceType==8){wcscat(cl, L" ");\
+ * while(fgetws(cl+wcslen(cl),4096,stdin)!=NULL)continue;SetEnvironmentVariableW(L"QPRCMDLINE",cl+17);}
+ * Build: For fun code is changed from standard main(argc,*argv[]) to something like https://nullprogram.com/blog/2016/01/31/ and https://scorpiosoftware.net/2023/03/16/minimal-executables/)
+ * x86_64-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -municode -Wall -Wextra -mno-stack-arg-probe \
+   -finline-limit=256 -Wl,-gc-sections -Xlinker --stack=0x100000,0x100000 mainv1.c -nostdlib -lucrtbase -lkernel32 -lntdll -s -o powershell64.exe && strip -R .reloc powershell64.exe
+   i686-w64-mingw32-gcc -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables -mno-stack-arg-probe -municode -Wall -Wextra -mno-stack-arg-probe \
+   -finline-limit=256 -Wl,-gc-sections -Xlinker --stack=0x100000,0x100000 mainv1.c -nostdlib -lucrtbase -lkernel32 -lntdll -s -o powershell32.exe && strip -R .reloc  powershell32.exe
  */
  
 #include <wchar.h>
@@ -25,11 +31,11 @@
 /* clbuf = incoming cmdline */
 #define clbuf peb->ProcessParameters->CommandLine.Buffer
 /* copied from dlls/ntdll/wcstring.c, just to make the binary another 0,5 kB smaller... */ 
-static __attribute__((noinline)) LPWSTR _wcschr(LPCWSTR str, WCHAR ch) { do { if (*str == ch) return (WCHAR*)(ULONG_PTR)str; } while (*str++); return NULL; }
+static __attribute__((noinline)) LPWSTR _wcschr(LPCWSTR s, WCHAR ch) { do { if (*s == ch) return (WCHAR*)(ULONG_PTR)s; } while (*s++); return NULL;}
 /* for e.g. -noprofile, -nologo , -mta etc */
-static BOOL is_single_option(WCHAR* opt) { return !!_wcschr(L"nNmMsS", opt[1]); }
-/* no new options may follow -c, -f , -e (but not -ex(ecutionpolicy)! and '-') */
-static BOOL is_last_option(WCHAR* opt) { return ((_wcschr(L"cCfFeE\0", opt[1]) && _wcsnicmp(&opt[1], L"ex", 2) && _wcsnicmp(&opt[1], L"config", 6))); }
+static BOOL is_single_option(WCHAR* s) { return !!_wcschr(L"nNmMsS", s[1]); }
+/* no new options may follow -c, -f , -e or - , (but not -ex(ecutionpolicy)! and -config for which simple checks are added)  */
+static BOOL is_last_option(WCHAR* s) { return _wcschr(L"cCfFeE\0", s[1]) ? ( s[2] != L'x' && s[2] != L'X' && s[6] != L'g' && s[6] != L'G' ? 1 : 0) : 0; }
 /* join strings with a space in between */
 static __attribute__((noinline)) void join(WCHAR* string1, WCHAR* string2) { if (string2) wcscat(wcscat(string1, L" "), string2); }
 
@@ -39,18 +45,18 @@ int mainCRTStartup(PPEB peb) {
     STARTUPINFOW si = {0};
     PROCESS_INFORMATION pi = {0};
 
-    WCHAR* cmd = (clbuf[0] == L'"') ? _wcschr(clbuf + 1, L'"') + 1 : _wcschr(clbuf, L' '); /* skip arg[0] to get the cmdline to be executed */
-    while (token) {                                                                        /* get the filename */
-        token = wcstok_s(NULL, L"\\", &ptr);
-        if (!*ptr) break;
-    }
+    WCHAR* cmd = (clbuf[0] == L'"') ? _wcschr(clbuf + 1, L'"') + 1 : _wcschr(clbuf, L' ');            /* skip arg[0] to get the cmdline to be executed */
+    if (!_wcsnicmp(cmd + 1, L"-v", 2)) cmd = (cmd = _wcschr(++cmd, L' ')) ? _wcschr(++cmd, L' ') : 0; /* skip incompatible version option, like '-version 3.0' */
+    do { token = wcstok_s(NULL, L"\\", &ptr); } while (token && *ptr);                                /* get the filename */
     wcstok_s(token, L".", &ptr);
+    
     /* I can also act as a dummy program if my exe-name is not powershell, allows to replace a system exe (like wusa.exe, or any exe really) by a function in profile.ps1 */
     if (_wcsnicmp(token, L"Powershell", 10)) {         /* note: set desired exitcode in the function in profile.ps1 */
         SetEnvironmentVariableW(L"QPRCMDLINE", cmd);   /* track cmdline via $env:QPRCMDLINE ($MyInvocation.Line seems to remove quotes (!)) */
         wcscat(wcscat(cl, L"-nop -c QPR."), token);    /* add some prefix to the exe and execute it through pwsh , so we can query for program replacement in profile.ps1 */
     } else {
         token = (cmd ? wcstok_s(cmd, L" ", &ptr) : 0); /* Start breaking up cmdline to look for options */
+
         /* Main program: pwsh requires a command option "-c" , powershell doesn`t;  insert it e.g. 'powershell -nologo 2+1' should go into 'powershell -nologo -c 2+1'*/
         while (token) {                                /* Break up cmdline manually (as CommandLineToArgVW seems to remove some (double) qoutes) */
             if (token[0] == L'/') token[0] = L'-';     /* deprecated '/' still works in powershell 5.1, replace to simplify code */
@@ -62,20 +68,14 @@ int mainCRTStartup(PPEB peb) {
                 join(cl, ptr);   /* add remainder of cmdline and done */
                 break;
             }
-            if (is_single_option(token)) {                         /* e.g. -noprofile, -nologo , -mta etc */
-                if (_wcsnicmp(token, L"-nop", 4)) join(cl, token); /* skip -noprofile to alays enable hacks in profile.ps1 */
-            } else {                                               /* assuming option + argument (e.g. '-executionpolicy bypass') AND a valid command!!!, no check for garbage commands!!!!!! */
-                if (!_wcsnicmp(token, L"-ve", 3))                  /* skip incompatible version option, like '-version 3.0' */
-                    token = wcstok_s(NULL, L" ", &ptr);
-                else {
-                    join(cl, token);                               /* concatenate option + arg for option with argument */
-                    token = wcstok_s(NULL, L" ", &ptr);
-                    join(cl, token);
-                }
-            }
+            if (_wcsnicmp(token, L"-nop", 4))    /* skip -noprofile to always enable hacks in profile.ps1 */
+                join(cl, token);           
+            if (!is_single_option(token))
+                join(cl, token = wcstok_s(NULL, L" ", &ptr));
             token = wcstok_s(NULL, L" ", &ptr);
         }
     }
+    
     // /*track the cmd:*/ FILE *fptr; fptr = fopen("c:\\log.txt", "a");fputws(L"used commandline is now: ",fptr); fputws(cl,fptr); fclose(fptr);
     CreateProcessW(!_wgetenv(L"PS51") ? _wgetenv(L"PS7") : L"c:\\Windows\\system32\\WindowsPowershell\\v1.0\\PS51.exe", cl, 0, 0, 0, 0, 0, 0, &si, &pi);
     WaitForSingleObject(pi.hProcess, INFINITE); GetExitCodeProcess(pi.hProcess, &exitcode); CloseHandle(pi.hProcess); CloseHandle(pi.hThread);
